9eb9358292a801f21944202037951e495f0ffd6d
Stimulationparalysis <jealousbeastim@yahoo.co.jp>
2019-05-03 21:32:57 +0900

    グループ作成機能
    グループ作成機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,6 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+    params.require(:group).permit(:name, { :group_id => [] })
   end
 end
