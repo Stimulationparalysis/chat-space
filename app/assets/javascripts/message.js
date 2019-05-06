08dfe4c16dc07ca825b965adcfe2c8174e6c9eef
Stimulationparalysis <jealousbeastim@yahoo.co.jp>
2019-05-06 20:42:10 +0900

    メッセージ送信（非同期通信）
    メッセージ送信機能に非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,47 @@
+$(function(){
+  function buildHTML(message){
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class='message'>
+                  <div class='upper-info'>
+                    <p class='upper-info__user'>
+                      ${message.user_name}
+                    </p>
+                    <p class='upper-info__date'>
+                      ${message.date}
+                    </p>
+                  </div>
+                  <div class='meesage__text'>
+                    <p class='lower-message__content'>
+                      ${content}
+                    </p>
+                      ${img}
+                  </div>
+                </div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var $this = $(this);
+    var formData = new FormData(this);
+    var href = window.location.href;
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $this.get(0).reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function(){
+      alert('error');
+    });
+    return false
+  });
+});
