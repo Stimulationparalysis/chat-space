458a7d5a555d488171d47c249bd616d67f71f9a0
Stimulationparalysis <jealousbeastim@yahoo.co.jp>
2019-05-06 21:12:40 +0900

    Message.js修正
    Message.jsの関数と引数の名前を修正。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,5 @@
 $(function(){
-  function buildHTML(message){
+  function buildMessageHTML(message){
     var content = message.content ? `${ message.content }` : "";
     var img = message.image ? `<img src= ${ message.image }>` : "";
     var html = `<div class='message'>
@@ -33,8 +33,8 @@ $(function(){
       processData: false,
       contentType: false
     })
-    .done(function(data){
-      var html = buildHTML(data);
+    .done(function(jbuilderData){
+      var html = buildMessageHTML(jbuilderData);
       $('.messages').append(html)
       $this.get(0).reset();
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
